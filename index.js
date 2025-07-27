29e6e3ff68bf9d9006c71a0d555545e969fd9921
Yaroslav-Hoian <sgoyan1405@gmail.com>
2025-07-27 13:08:21 +0000

    Deploying to gh-pages from @ Beowulf3/market@0b9413c72b049c362788d75cf7bb2bd12a8989e7 🚀

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-import"./assets/styles-JE8YjOlG.js";import{a as r}from"./assets/vendor-N5iQpiFS.js";async function s(){try{return(await r("https://dummyjson.com/products/category-list")).data}catch(t){throw console.log(t.message),new Error("Something went wrong!")}}const a={categotiesList:document.querySelector(".categories"),productsList:document.querySelector(".products")};function n(t){const e=["All",...t].map(o=>`
+import"./assets/styles-JE8YjOlG.js";import{a as e}from"./assets/vendor-N5iQpiFS.js";async function d(){try{return(await e("https://dummyjson.com/products/category-list")).data}catch(t){throw console.log(t.message),new Error("Something went wrong!")}}async function u(t=1){try{return(await e.get(`https://dummyjson.com/products?limit=12&skip=${(t-1)*12}`)).data}catch(s){throw console.log(s.message),new Error("Something went wrong!")}}const c={categotiesList:document.querySelector(".categories"),productsList:document.querySelector(".products")};function l(t){const s=["All",...t].map(o=>`
     <li class="categories__item">
         <button class="categories__btn" type="button">${o}</button>
     </li>
- `).join("");a.categotiesList.insertAdjacentHTML("beforeend",e)}async function c(){n(await s())}c();
+ `).join("");c.categotiesList.insertAdjacentHTML("beforeend",s)}function m(t){const s=t.map(({id:o,title:r,thumbnail:a,brand:n,category:i,price:p})=>`
+    <li class="products__item" data-id="${o}">
+    <img class="products__image" src="${a}" alt="${r}"/>
+    <p class="products__title">${r}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:${n}</span></p>
+    <p class="products__category">Category:${i}</p>
+    <p class="products__price">Price:${p}$</p>
+ </li>
+    `).join("");c.productsList.insertAdjacentHTML("beforeend",s)}async function g(){l(await d());const t=await u();m(t.products)}g();
 //# sourceMappingURL=index.js.map
